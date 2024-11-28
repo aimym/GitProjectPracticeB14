746575253f1c2e0a083aaaec73c486417f0a0952
aimym <aidananuri25@gmail.com>
2024-11-28 13:23:24 -0500

    new changes

--- src/com/syntax/Class3.java
+++ src/com/syntax/Class3.java
@@ -3,5 +3,6 @@ package com.syntax;
 public class Class3 {
     public static void main(String[] args) {
         System.out.println("This is a code in a class 3");
+        System.out.println("One more change");
     }
 }
--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         System.out.println("version is complete");
         System.out.println("Hey hey hey");
+        System.out.println("change");
 
     }
 }
